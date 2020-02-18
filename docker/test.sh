d5d7c359ccab22fddc718269c46f134f5e42c3b3
Trent Mohay <trent.mohay@consensys.net>
2020-02-18 16:21:50 +1100

    First draft hello world

---
+++ docker/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e
+
+export GOSS_PATH=tests/goss-linux-amd64
+export GOSS_OPTS="$GOSS_OPTS --format junit"
+export GOSS_FILES_STRATEGY=cp
+DOCKER_IMAGE=$1
+
+i=0
+
+# Test for normal startup
+GOSS_FILES_PATH=tests/01 \
+bash tests/dgoss run $DOCKER_IMAGE \
+> ./reports/01.xml || i=`expr $i + 1`
+
+exit $i
