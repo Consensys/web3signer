692c14bf5985213e5306bf84fcffc1ab5d34527e
Trent Mohay <trent.mohay@consensys.net>
2020-02-25 14:54:58 +1100

    check both block and attestation signing

--- acceptance-tests/src/test/java/tech/pegasys/eth2signer/dsl/signer/Signer.java
+++ acceptance-tests/src/test/java/tech/pegasys/eth2signer/dsl/signer/Signer.java
@@ -100,7 +100,7 @@ public class Signer {
     return "OK".equals(body);
   }
 
-  public HttpResponse signData(final PublicKey publicKey, final Bytes message, final Bytes domain)
+  public HttpResponse signData(final String signingEndpoint, final PublicKey publicKey, final Bytes message, final Bytes domain)
       throws ExecutionException, InterruptedException {
     final SigningRequestBody requestBody =
         new SigningRequestBody(publicKey.toString(), message.toHexString(), domain.toHexString());
@@ -109,7 +109,7 @@ public class Signer {
     final CompletableFuture<HttpResponse> responseBodyFuture = new CompletableFuture<>();
     final HttpClientRequest request =
         httpClient.post(
-            "/signer/block",
+            "/signer/" + signingEndpoint,
             response ->
                 response.bodyHandler(
                     body ->
