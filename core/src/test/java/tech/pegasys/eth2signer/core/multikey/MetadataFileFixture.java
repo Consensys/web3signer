627304938957830d68d498de86a50384712c5f0b
Jason Frame <909467+jframe@users.noreply.github.com>
2020-03-02 14:21:19 +1000

    removed unused text fixture values

--- core/src/test/java/tech/pegasys/eth2signer/core/multikey/MetadataFileFixture.java
+++ core/src/test/java/tech/pegasys/eth2signer/core/multikey/MetadataFileFixture.java
@@ -14,7 +14,6 @@ package tech.pegasys.eth2signer.core.multikey;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
@@ -24,22 +23,6 @@ public class MetadataFileFixture {
 
   public static final String CONFIG_FILE_EXTENSION = ".yaml";
   public static String LOWERCASE_ADDRESS = "627306090abab3a6e1400e9345bc60c78a8bef57";
-  public static String PREFIX_ADDRESS = "fe3b557e8fb62b89f4916b721be55ceb828dbd73";
-  public static String UNKNOWN_TYPE_SIGNER_ADDRESS = "fe3b557e8fb62b89f4916b721be55ceb828dbd75";
-  public static String UNKNOWN_TYPE_SIGNER_FILENAME =
-      "unknown_type_signer_" + UNKNOWN_TYPE_SIGNER_ADDRESS + CONFIG_FILE_EXTENSION;
-  public static String MISSING_KEY_PATH_ADDRESS = "fe3b557e8fb62b89f4916b721be55ceb828dbd76";
-  public static String MISSING_PASSWORD_PATH_ADDRESS = "fe3b557e8fb62b89f4916b721be55ceb828dbd77";
-  public static String MISSING_KEY_AND_PASSWORD_PATH_ADDRESS =
-      "fe3b557e8fb62b89f4916b721be55ceb828dbd78";
-  public static String MISSING_KEY_PATH_FILENAME =
-      "missing_key_file_" + MISSING_KEY_PATH_ADDRESS + CONFIG_FILE_EXTENSION;
-  public static String MISSING_PASSWORD_PATH_FILENAME =
-      "missing_password_file_" + MISSING_PASSWORD_PATH_ADDRESS + CONFIG_FILE_EXTENSION;
-  public static String MISSING_KEY_AND_PASSWORD_PATH_FILENAME =
-      "missing_key_and_password_file_"
-          + MISSING_KEY_AND_PASSWORD_PATH_ADDRESS
-          + CONFIG_FILE_EXTENSION;
 
   public static String PREFIX_MIXEDCASE_ADDRESS = "f17f52151ebef6c7334fad080c5704d77216b732";
   public static String PREFIX_MIXEDCASE_FILENAME =
@@ -52,13 +35,6 @@ public class MetadataFileFixture {
       "duplicate_foo_" + PREFIX_LOWERCASE_DUPLICATE_ADDRESS + CONFIG_FILE_EXTENSION;
   public static String PREFIX_LOWERCASE_DUPLICATE_FILENAME_2 =
       "duplicate_bar_" + PREFIX_LOWERCASE_DUPLICATE_ADDRESS + CONFIG_FILE_EXTENSION;
-
-  public static String KEY_FILE = "/path/to/k.key";
-  public static String PASSWORD_FILE = "/path/to/p.password";
-  public static String KEY_FILE_2 = "/path/to/k2.key";
-  public static String PASSWORD_FILE_2 = "/path/to/p2.password";
-
-  private static final URL metadataTomlConfigPathUrl = Resources.getResource("metadata-configs");
 
   static void copyMetadataFileToDirectory(
       final Path configsDirectory, final String metadataFilename) {
