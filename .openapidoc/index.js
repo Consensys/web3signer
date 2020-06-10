b1a2db3c1e8a009f687595e25e1e1d76ff693a42
Usman Saleem <usman@usmans.info>
2020-06-11 08:15:52 +1000

    error handling and code formatting
    Signed-off-by: Usman Saleem <usman@usmans.info>

--- .openapidoc/index.js
+++ .openapidoc/index.js
@@ -1,87 +1,101 @@
-const fs = require('fs');
-const yaml = require('js-yaml');
-const {https} = require('follow-redirects');
+const fs = require("fs");
+const yaml = require("js-yaml");
+const { https } = require("follow-redirects");
 
-const spec = '../core/build/resources/main/openapi/eth2signer.yaml'
-const versionsFileUrl = 'https://github.com/PegaSysEng/eth2signer/raw/gh-pages/versions.json'
-const distDir = './dist/'
-const versionsFile = distDir + 'versions.json'
-
+const spec = "../core/build/resources/main/openapi/eth2signer.yaml";
+const versionsFileUrl =
+  "https://github.com/PegaSysEng/eth2signer/raw/gh-pages/versions.json";
+const distDir = "./dist/";
+const versionsFile = distDir + "versions.json";
 
 async function downloadAsString(source) {
-    return new Promise((resolve, reject) => {
-        let output = ''
-        return https.get(source, response => {
-            response.on('data', chunk => (output += chunk))
-            response.on('end', () => resolve(output))
-        }).on('error', err => {
-           reject(err)
-        });
-    })
+  return new Promise((resolve, reject) => {
+    let output = "";
+    return https
+      .get(source, (response) => {
+        const { statusCode, headers } = response;
+        if (statusCode >= 400)
+          return reject(
+            new Error(
+              `Failed to download file "${source}". Status: ${statusCode}`
+            )
+          );
+
+        response.on("data", (chunk) => (output += chunk));
+        response.on("end", () => resolve(output));
+      })
+      .on("error", (err) => {
+        return reject(err);
+      });
+  });
 }
 
 function ensureDistDir() {
-    fs.mkdirSync(distDir, { recursive: true })
+  fs.mkdirSync(distDir, { recursive: true });
 }
 
 function cleanDistDir() {
-    fs.rmdirSync(distDir, { recursive: true });
+  fs.rmdirSync(distDir, { recursive: true });
 }
 
 function calculateVersionFromSpec() {
-    let data = yaml.safeLoad(fs.readFileSync(spec, 'utf8'));
-    return data.info.version;
+  let data = yaml.safeLoad(fs.readFileSync(spec, "utf8"));
+  return data.info.version;
 }
 
 function isReleaseVersion(specVersion) {
-    return !specVersion.includes('-dev-')
+  return !specVersion.includes("-dev-");
 }
 
 function copySpecToDist(specVersion) {
-    fs.copyFileSync(spec, distDir + 'eth2signer-latest.yaml')
+  fs.copyFileSync(spec, distDir + "eth2signer-latest.yaml");
 
-    if (isReleaseVersion(specVersion)) {
-        fs.copyFileSync(spec, `${distDir}eth2signer-${specVersion}.yaml`)
-    }            
+  if (isReleaseVersion(specVersion)) {
+    fs.copyFileSync(spec, `${distDir}eth2signer-${specVersion}.yaml`);
+  }
 }
 
 function updateVersionJson(specVersion, versionJsonString) {
-    var versions = JSON.parse(versionJsonString);
-    versions[specVersion] = {spec: specVersion, source: specVersion}
-    versions['stable'] = {spec: specVersion, source: specVersion}
-    fs.writeFileSync(versionsFile, JSON.stringify(versions,null,1));
+  var versions = JSON.parse(versionJsonString);
+  versions[specVersion] = { spec: specVersion, source: specVersion };
+  versions["stable"] = { spec: specVersion, source: specVersion };
+  fs.writeFileSync(versionsFile, JSON.stringify(versions, null, 1));
 }
 
-async function downloadAndUpdateReleaseVersion(specVersion) {
-    return new Promise((resolve, reject) => {
-        if (!isReleaseVersion(specVersion)) {
-            console.log("Not a release version, bypassing updating versions.json")
-            return resolve(false)
-        }
+async function downloadAndUpdateVersionsFile(specVersion) {
+  return new Promise((resolve, reject) => {
+    if (!isReleaseVersion(specVersion)) {
+      console.log("Not a release version, bypassing updating versions.json");
+      return resolve(false);
+    }
 
-        return downloadAsString(versionsFileUrl).then(value => {
-            try {
-                updateVersionJson(specVersion, value)
-                return resolve(true)
-            } catch(err) {
-                return reject(err)
-            }
-        })
-    });
+    return downloadAsString(versionsFileUrl)
+      .then((value) => {
+        try {
+          updateVersionJson(specVersion, value);
+          return resolve(true);
+        } catch (err) {
+          return reject(err);
+        }
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  });
 }
 
 async function run() {
-    cleanDistDir()
-    ensureDistDir()
+  cleanDistDir();
+  ensureDistDir();
 
-    let specVersion = calculateVersionFromSpec()
-    console.log("Spec Version: " + specVersion)   
-    
-    copySpecToDist(specVersion)
-    await downloadAndUpdateReleaseVersion(specVersion)
+  let specVersion = calculateVersionFromSpec();
+  console.log("Spec Version: " + specVersion);
+
+  copySpecToDist(specVersion);
+  await downloadAndUpdateVersionsFile(specVersion);
 }
 
-run().catch(error => {
-    console.log("OpenAPI publish failed")
-    console.log(error.message)
-})
+run().catch((error) => {
+  console.log("OpenAPI spec publish failed");
+  console.log(error.message);
+});
