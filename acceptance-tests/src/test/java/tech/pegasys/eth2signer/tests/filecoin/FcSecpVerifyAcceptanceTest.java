a347a104a1d9f96ee3963cc31d18ae91d577803c
Jason Frame <909467+jframe@users.noreply.github.com>
2020-08-25 16:19:04 +1000

    Fc secp verify AT

--- acceptance-tests/src/test/java/tech/pegasys/eth2signer/tests/filecoin/FcSecpVerifyAcceptanceTest.java
+++ acceptance-tests/src/test/java/tech/pegasys/eth2signer/tests/filecoin/FcSecpVerifyAcceptanceTest.java
@@ -19,18 +19,15 @@ import static org.hamcrest.Matchers.equalTo;
 
 import tech.pegasys.eth2signer.core.service.jsonrpc.FcJsonRpc;
 import tech.pegasys.eth2signer.core.service.jsonrpc.FilecoinSignature;
-import tech.pegasys.eth2signer.core.signing.BlsArtifactSignature;
-import tech.pegasys.eth2signer.core.signing.FcBlsArtifactSigner;
 import tech.pegasys.eth2signer.core.signing.KeyType;
 import tech.pegasys.eth2signer.core.signing.filecoin.FilecoinAddress;
 import tech.pegasys.eth2signer.core.signing.filecoin.FilecoinNetwork;
 import tech.pegasys.eth2signer.dsl.signer.SignerConfigurationBuilder;
 import tech.pegasys.eth2signer.dsl.utils.MetadataFileHelpers;
 import tech.pegasys.eth2signer.tests.AcceptanceTestBase;
-import tech.pegasys.teku.bls.BLSKeyPair;
-import tech.pegasys.teku.bls.BLSPublicKey;
-import tech.pegasys.teku.bls.BLSSecretKey;
 
+import java.io.File;
+import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.util.Base64;
 import java.util.List;
@@ -40,36 +37,37 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ValueNode;
 import com.github.arteam.simplejsonrpc.core.domain.Request;
+import com.google.common.io.Resources;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.apache.tuweni.bytes.Bytes;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
-public class FcBlsVerifyAcceptanceTest extends AcceptanceTestBase {
-  private static final String dataString =
-      Base64.getEncoder().encodeToString("Hello World".getBytes(UTF_8));
-  private static final String PRIVATE_KEY =
-      "5abc1334d98d1432150df310f9d2fd51780a2b8a7489891f5d4ab9e77e6fb169";
-
+public class FcSecpVerifyAcceptanceTest extends AcceptanceTestBase {
   protected @TempDir Path testDirectory;
-  private static final MetadataFileHelpers metadataFileHelpers = new MetadataFileHelpers();
-  private static final BLSSecretKey key = BLSSecretKey.fromBytes(Bytes.fromHexString(PRIVATE_KEY));
-  private static final BLSKeyPair keyPair = new BLSKeyPair(key);
-  private static final BLSPublicKey publicKey = keyPair.getPublicKey();
-  private static final FilecoinNetwork network = FilecoinNetwork.TESTNET;
-  private static final FcBlsArtifactSigner signatureGenerator =
-      new FcBlsArtifactSigner(keyPair, network);
-  private static final BlsArtifactSignature expectedSignature =
-      signatureGenerator.sign(Bytes.fromBase64String(dataString));
 
-  final FilecoinAddress identifier = FilecoinAddress.blsAddress(publicKey.toBytes());
+  private final MetadataFileHelpers metadataFileHelpers = new MetadataFileHelpers();
+
+  // Public Key of Keystore stored in resource "secp256k1/wallet.json"
+  public static final String PUBLIC_KEY_HEX_STRING =
+      "09b02f8a5fddd222ade4ea4528faefc399623af3f736be3c44f03e2df22fb792f3931a4d9573d333ca74343305762a753388c3422a86d98b713fc91c1ea04842";
+  final String dataString = Base64.getEncoder().encodeToString("Hello World".getBytes(UTF_8));
+  final String expectedBase64Signature =
+      "rapLdQPn5RQkiPeG6ij/0cCSBbsvHgO1Mx/rZVyM+HUrpdjamK6QSwSHzobfTaF2ehGevZ31t4XwZc7M5SnPwwA=";
+  final FilecoinAddress identifier =
+      FilecoinAddress.secpAddress(Bytes.fromHexString("04" + PUBLIC_KEY_HEX_STRING));
 
   @Test
-  void receiveTrueResponseWhenSubmitValidVerifyRequestToFilecoinEndpoint() {
-    final String configFilename = publicKey.toString().substring(2);
-    final Path keyConfigFile = testDirectory.resolve(configFilename + ".yaml");
-    metadataFileHelpers.createUnencryptedYamlFileAt(keyConfigFile, PRIVATE_KEY, KeyType.BLS);
+  void receiveTrueResponseWhenSubmitValidVerifyRequestToFilecoinEndpoint()
+      throws URISyntaxException {
+    final String keyPath =
+        new File(Resources.getResource("secp256k1/wallet.json").toURI()).getAbsolutePath();
+
+    final Path keyConfigFile = testDirectory.resolve("arbitrary_secp.yaml");
+
+    metadataFileHelpers.createKeyStoreYamlFileAt(
+        keyConfigFile, Path.of(keyPath), "pass", KeyType.SECP256K1);
 
     final SignerConfigurationBuilder builder = new SignerConfigurationBuilder();
     builder.withKeyStoreDirectory(testDirectory);
@@ -79,11 +77,11 @@ public class FcBlsVerifyAcceptanceTest extends AcceptanceTestBase {
     ObjectMapper mapper = new ObjectMapper();
 
     final FilecoinSignature filecoinSignature =
-        new FilecoinSignature(
-            FcJsonRpc.BLS_VALUE, expectedSignature.getSignatureData().toBytes().toBase64String());
+        new FilecoinSignature(FcJsonRpc.SECP_VALUE, expectedBase64Signature);
     final JsonNode params =
         mapper.convertValue(
-            List.of(identifier.encode(network), dataString, filecoinSignature), JsonNode.class);
+            List.of(identifier.encode(FilecoinNetwork.TESTNET), dataString, filecoinSignature),
+            JsonNode.class);
 
     final Request request = new Request("2.0", "Filecoin.WalletVerify", params, id);
     final Response response =
