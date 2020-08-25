3a83a643e846e0a8458cb5efe5604cf4d0c682db
Jason Frame <909467+jframe@users.noreply.github.com>
2020-08-25 12:11:11 +1000

    implement verify operation for ArtifactSigner

--- core/src/main/java/tech/pegasys/eth2signer/core/signing/EthSecpArtifactSigner.java
+++ core/src/main/java/tech/pegasys/eth2signer/core/signing/EthSecpArtifactSigner.java
@@ -12,12 +12,20 @@
  */
 package tech.pegasys.eth2signer.core.signing;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static tech.pegasys.eth2signer.core.service.operations.IdentifierUtils.normaliseIdentifier;
 
 import tech.pegasys.signers.secp256k1.EthPublicKeyUtils;
+import tech.pegasys.signers.secp256k1.api.Signature;
 import tech.pegasys.signers.secp256k1.api.Signer;
 
+import java.math.BigInteger;
+
 import org.apache.tuweni.bytes.Bytes;
+import org.web3j.crypto.ECDSASignature;
+import org.web3j.crypto.Hash;
+import org.web3j.crypto.Sign;
+import org.web3j.utils.Numeric;
 
 public class EthSecpArtifactSigner implements ArtifactSigner {
 
@@ -35,5 +43,24 @@ public class EthSecpArtifactSigner implements ArtifactSigner {
   @Override
   public SecpArtifactSignature sign(final Bytes message) {
     return new SecpArtifactSignature(signer.sign(message.toArray()));
+  }
+
+  @Override
+  public boolean verify(final Bytes message, final ArtifactSignature signature) {
+    checkArgument(signature instanceof SecpArtifactSignature);
+    final SecpArtifactSignature secpArtifactSignature = (SecpArtifactSignature) signature;
+    final Signature signatureData = secpArtifactSignature.getSignatureData();
+
+    final ECDSASignature initialSignature =
+        new ECDSASignature(signatureData.getR(), signatureData.getS());
+    final ECDSASignature canonicalSignature = initialSignature.toCanonicalised();
+
+    final int recId = signatureData.getV().intValue();
+    final byte[] digest = Hash.sha3(message.toArrayUnsafe());
+    final BigInteger signaturePublicKey =
+        Sign.recoverFromSignature(recId, canonicalSignature, digest);
+    final BigInteger expectedPublicKey =
+        Numeric.toBigInt(EthPublicKeyUtils.toByteArray(signer.getPublicKey()));
+    return signaturePublicKey.equals(expectedPublicKey);
   }
 }
--- core/src/main/java/tech/pegasys/eth2signer/core/signing/FcBlsArtifactSigner.java
+++ core/src/main/java/tech/pegasys/eth2signer/core/signing/FcBlsArtifactSigner.java
@@ -12,6 +12,7 @@
  */
 package tech.pegasys.eth2signer.core.signing;
 
+import static com.google.common.base.Preconditions.checkArgument;
 import static tech.pegasys.teku.bls.hashToG2.HashToCurve.hashToG2;
 
 import tech.pegasys.eth2signer.core.signing.filecoin.FilecoinAddress;
@@ -43,10 +44,19 @@ public class FcBlsArtifactSigner implements ArtifactSigner {
   }
 
   @Override
-  public BlsArtifactSignature sign(final Bytes data) {
-    final G2Point hashInGroup2 = new G2Point(hashToG2(data, DST));
+  public BlsArtifactSignature sign(final Bytes message) {
+    final G2Point hashInGroup2 = new G2Point(hashToG2(message, DST));
     final G2Point g2Point = keyPair.getSecretKey().getSecretKey().sign(hashInGroup2);
     final BLSSignature blsSignature = new BLSSignature(new Signature(g2Point));
     return new BlsArtifactSignature(blsSignature);
+  }
+
+  @Override
+  public boolean verify(final Bytes message, final ArtifactSignature artifactSignature) {
+    checkArgument(artifactSignature instanceof BlsArtifactSignature);
+    final BlsArtifactSignature blsArtifactSignature = (BlsArtifactSignature) artifactSignature;
+    final Signature signature = blsArtifactSignature.getSignatureData().getSignature();
+    final G2Point hashInGroup2 = new G2Point(hashToG2(message, DST));
+    return signature.verify(keyPair.getPublicKey().getPublicKey(), hashInGroup2);
   }
 }
