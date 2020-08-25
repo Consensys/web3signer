8394d67f42fce49c89e0f4602a84519ddad632b0
Trent Mohay <trent.mohay@consensys.net>
2020-08-25 13:00:09 +1000

    fix spotless

--- acceptance-tests/src/test/java/tech/pegasys/eth2signer/tests/filecoin/FcSignMessageAcceptanceTest.java
+++ acceptance-tests/src/test/java/tech/pegasys/eth2signer/tests/filecoin/FcSignMessageAcceptanceTest.java
@@ -30,7 +30,6 @@ import java.nio.file.Path;
 import java.util.HashMap;
 import java.util.Map;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
