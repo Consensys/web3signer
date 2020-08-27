47c023859c37f73285da3379c55399e3e11463b7
Trent Mohay <trent.mohay@consensys.net>
2020-08-27 11:05:08 +1000

    Move cbor into separate class

--- core/src/main/java/tech/pegasys/eth2signer/core/service/jsonrpc/FcJsonRpc.java
+++ core/src/main/java/tech/pegasys/eth2signer/core/service/jsonrpc/FcJsonRpc.java
@@ -12,29 +12,19 @@
  */
 package tech.pegasys.eth2signer.core.service.jsonrpc;
 
-import static com.fasterxml.jackson.dataformat.cbor.CBORConstants.PREFIX_TYPE_INT_POS;
-import static com.fasterxml.jackson.dataformat.cbor.CBORConstants.SUFFIX_UINT64_ELEMENTS;
-
 import tech.pegasys.eth2signer.core.signing.ArtifactSignature;
 import tech.pegasys.eth2signer.core.signing.ArtifactSigner;
 import tech.pegasys.eth2signer.core.signing.ArtifactSignerProvider;
 import tech.pegasys.eth2signer.core.signing.BlsArtifactSignature;
 import tech.pegasys.eth2signer.core.signing.SecpArtifactSignature;
-import tech.pegasys.eth2signer.core.signing.filecoin.FilecoinAddress;
 import tech.pegasys.eth2signer.core.signing.filecoin.exceptions.FilecoinSignerNotFoundException;
-import tech.pegasys.eth2signer.core.util.Blake2b;
 import tech.pegasys.eth2signer.core.util.ByteUtils;
 import tech.pegasys.signers.secp256k1.api.Signature;
 import tech.pegasys.teku.bls.BLSSignature;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.math.BigInteger;
 import java.util.Optional;
 import java.util.Set;
 
-import com.fasterxml.jackson.dataformat.cbor.CBORFactory;
-import com.fasterxml.jackson.dataformat.cbor.CBORGenerator;
 import com.github.arteam.simplejsonrpc.core.annotation.JsonRpcMethod;
 import com.github.arteam.simplejsonrpc.core.annotation.JsonRpcParam;
 import com.github.arteam.simplejsonrpc.core.annotation.JsonRpcService;
@@ -42,7 +32,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
-import org.apache.tuweni.units.bigints.UInt64;
 import org.web3j.utils.Numeric;
 
 @JsonRpcService
@@ -102,84 +91,13 @@ public class FcJsonRpc {
       @JsonRpcParam("identifier") final String identifier,
       @JsonRpcParam("message") final FilecoinMessage message) {
 
-    final Bytes cborEncodedBytes = cborEncode(message);
-    final Bytes messageHash = Blake2b.sum256(cborEncodedBytes);
-    final Bytes encodedHashAndCode = encodeHashWithCode(messageHash);
-    final Bytes fcCid = createFilecoinCid(encodedHashAndCode);
+    final FcMessageEncoder encoder = new FcMessageEncoder();
+    final Bytes fcCid = encoder.createFilecoinCid(message);
 
     final FilecoinSignature signature =
         filecoinWalletSign(identifier, Bytes.wrap(fcCid).toBase64String());
 
     return new FilecoinSignedMessage(message, signature);
-  }
-
-  private Bytes cborEncode(final FilecoinMessage message) {
-    final byte FILECOIN_MESSAGE_PREFIX = (byte) 138;
-    final CBORFactory cborFactory = new CBORFactory();
-    final ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-    try {
-      // need to ensure _cfgMinimalInts is set in cborgen ... no idea how.
-      final CBORGenerator gen = cborFactory.createGenerator(outputStream);
-      outputStream.write(FILECOIN_MESSAGE_PREFIX);
-      gen.writeNumber(message.getVersion());
-      gen.writeBinary(FilecoinAddress.decode(message.getTo()).getEncodedBytes().toArrayUnsafe());
-      gen.writeBinary(FilecoinAddress.decode(message.getFrom()).getEncodedBytes().toArrayUnsafe());
-      encodeUint64Value(message.getNonce(), gen);
-      serialiseBigInteger(message.getValue(), gen);
-      gen.writeNumber(message.getGasLimit());
-      serialiseBigInteger(message.getGasFeeCap(), gen);
-      serialiseBigInteger(message.getGasPremium(), gen);
-      encodeUint64Value(message.getMethod(), gen);
-      final Bytes paramBytes = Bytes.fromBase64String(message.getParams());
-      gen.writeBinary(null, paramBytes.toArrayUnsafe(), 0, paramBytes.size());
-      gen.close();
-      return Bytes.wrap(outputStream.toByteArray());
-    } catch (IOException e) {
-      throw new IllegalStateException("Failed to create cbor encoded message");
-    }
-  }
-
-  private void encodeUint64Value(final UInt64 value, final CBORGenerator gen) throws IOException {
-    if (value.fitsInt()) {
-      gen.writeNumber(value.intValue());
-    } else if (value.fitsLong()) {
-      gen.writeNumber(value.toLong());
-    } else {
-      final BigInteger bi = value.toBigInteger();
-      gen.writeRaw((byte) (PREFIX_TYPE_INT_POS + SUFFIX_UINT64_ELEMENTS));
-      final Bytes bytes = Bytes.wrap(bi.toByteArray());
-      final int numericBytes = bytes.size() - 1;
-      gen.writeBytes(bytes.slice(1, numericBytes).toArrayUnsafe(), 0, numericBytes);
-    }
-  }
-
-  // Roughly corresponds with filecoin:multhash:Encode
-  private Bytes encodeHashWithCode(final Bytes hashBytes) {
-    final BigInteger FC_HASHING_ALGO_CODE = BigInteger.valueOf(0xb220); // aka Blake2b-256
-    return Bytes.concatenate(
-        ByteUtils.putUVariant(FC_HASHING_ALGO_CODE),
-        ByteUtils.putUVariant(BigInteger.valueOf(hashBytes.size())),
-        hashBytes);
-  }
-
-  // Roughly corresponds to NewCidV1
-  private Bytes createFilecoinCid(final Bytes encodedHashAndCode) {
-    final byte CID_VERSION = (byte) 1;
-    final byte DagCBOR_CODEC_ID = (byte) 113;
-    return Bytes.concatenate(
-        Bytes.wrap(new byte[] {CID_VERSION}),
-        Bytes.wrap(new byte[] {DagCBOR_CODEC_ID}),
-        encodedHashAndCode);
-  }
-
-  private void serialiseBigInteger(final BigInteger value, final CBORGenerator gen)
-      throws IOException {
-    if (value.equals(BigInteger.ZERO)) {
-      gen.writeBinary(null, new byte[] {0}, 0, 0);
-    } else {
-      final byte[] bigIntBytes = value.toByteArray();
-      gen.writeBinary(null, bigIntBytes, 0, bigIntBytes.length);
-    }
   }
 
   @JsonRpcMethod("Filecoin.WalletVerify")
