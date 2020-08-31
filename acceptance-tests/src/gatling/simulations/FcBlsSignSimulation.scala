7955e18e8482bc39ff22423cb8d40e3f68f9dea5
Jason Frame <909467+jframe@users.noreply.github.com>
2020-09-01 08:08:24 +1000

    Update simulation to have bursts of signing requests

--- acceptance-tests/src/gatling/simulations/FcBlsSignSimulation.scala
+++ acceptance-tests/src/gatling/simulations/FcBlsSignSimulation.scala
@@ -1,8 +1,9 @@
 import java.nio.file.{Files, Path}
 import java.util
 
-import io.gatling.core.Predef._
+import io.gatling.core.Predef.{rampUsersPerSec, _}
 import io.gatling.http.Predef._
+import org.apache.tuweni.bytes.Bytes
 import tech.pegasys.eth2signer.dsl.lotus.FilecoinJsonRequests
 import tech.pegasys.eth2signer.dsl.signer.SignerConfigurationBuilder
 import tech.pegasys.eth2signer.dsl.signer.runner.Eth2SignerRunner
@@ -12,42 +13,43 @@ import scala.collection.JavaConverters.iterableAsScalaIterableConverter
 import scala.concurrent.duration.DurationInt
 import scala.language.postfixOps
 
-class FcSignSimulation extends Simulation {
+class FcBlsSignSimulation extends Simulation {
   private val keyStoreDirectory: Path = Files.createTempDirectory("bls")
   new MetadataFileHelpers().createRandomUnencryptedBlsKeys(keyStoreDirectory, 1000)
 
-  private val runner = Eth2SignerRunner.createRunner(new SignerConfigurationBuilder().withKeyStoreDirectory(keyStoreDirectory).build())
+  private val runner = Eth2SignerRunner.createRunner(new SignerConfigurationBuilder()
+    .withKeyStoreDirectory(keyStoreDirectory).build())
   runner.start()
   private val port: Int = runner.httpPort()
 
   private val httpProtocol = http.baseUrl("http://localhost:" + port)
 
-  private val jsonRpcClient = FilecoinJsonRequests.createJsonRpcClient("http://localhost:" + port + "/rpc/v1/filecoin")
+  private val baseUrl: String = "http://localhost:" + port
+  private val jsonRpcClient = FilecoinJsonRequests.createJsonRpcClient(baseUrl + "/rpc/v1/filecoin")
   private val addresses: util.List[String] = FilecoinJsonRequests.walletList(jsonRpcClient)
-  private val feeder = addresses.asScala.map(a => Map("address" -> a)).toArray.random
+  private val feeder = addresses.asScala.map(a => Map("address" -> a,
+    "data" -> Bytes.random(64).toHexString)).toArray.random
 
   private val signing = scenario("Signing")
     .feed(feeder)
-    .exec(http("request_1")
+    .exec(http("signingRequest")
 
       .post("/rpc/v1/filecoin")
       .body(StringBody(
-       """{
+        """{
           | "jsonrpc": "2.0",
           |  "method": "Filecoin.WalletSign",
-          |  "params":["${address}", ""],
+          |  "params":["${address}", "${data}"],
           |   "id": 1
           |}""".stripMargin)
-    ).asJson.check(jsonPath("$.result").not(""), jsonPath("$.result").notNull))
-    .pause(5)
+      ).asJson.check(jsonPath("$.result").not(""), jsonPath("$.result").notNull))
 
-  val targetTxRate = 100
-
+  val constantTxRate = 10
+  val burstTxRate = 100
   setUp(signing.inject(
-    nothingFor(10 seconds),
-    rampUsersPerSec(0) to targetTxRate during (10 seconds),
-    constantUsersPerSec(targetTxRate) during (1 minutes),
-    rampUsersPerSec(targetTxRate) to 0 during (10 seconds),
-    nothingFor(30 seconds)).protocols(httpProtocol))
-
+    constantUsersPerSec(constantTxRate) during (60 seconds),
+    rampUsersPerSec(burstTxRate) to 0 during (5 seconds),
+    constantUsersPerSec(constantTxRate) during (60 seconds),
+    rampUsersPerSec(burstTxRate) to 0 during (5 seconds),
+  ).protocols(httpProtocol))
 }
