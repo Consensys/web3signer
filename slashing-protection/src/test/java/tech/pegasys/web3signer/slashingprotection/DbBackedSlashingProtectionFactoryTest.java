7ad86bbe64060f0583b3c26070dfe1ce205723d0
Trent Mohay <trent.mohay@consensys.net>
2020-09-07 17:40:21 +1000

    added more testS

--- slashing-protection/src/test/java/tech/pegasys/web3signer/slashingprotection/DbBackedSlashingProtectionFactoryTest.java
+++ slashing-protection/src/test/java/tech/pegasys/web3signer/slashingprotection/DbBackedSlashingProtectionFactoryTest.java
@@ -17,12 +17,14 @@ package tech.pegasys.web3signer.slashingprotection;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.google.common.collect.Lists;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import java.nio.file.Path;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
+import org.assertj.db.type.Table;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
@@ -31,16 +33,8 @@ class DbBackedSlashingProtectionFactoryTest {
   @Test
   void dataBaseIsCreatedWithAppropriateTablesWhenInitialised(@TempDir Path testDir)
       throws SQLException {
-    final Path dbPath = testDir.toAbsolutePath().resolve("test.db");
-
-    assertThat(dbPath.toFile().exists()).isFalse();
-    final String sqlLitePath = "jdbc:sqlite:" + dbPath.toString();
-
-    DbBackedSlashingProtectionFactory.createDbBackedSlashingProtection(sqlLitePath, null, null);
-
-    assertThat(dbPath.toFile().exists()).isTrue();
-
-    final Connection conn = DriverManager.getConnection(sqlLitePath);
+    final String jdbcUrl = createSqliteDatabase(testDir);
+    final Connection conn = DriverManager.getConnection(jdbcUrl);
 
     final ResultSet rs =
         conn.createStatement().executeQuery("SELECT name FROM sqlite_master WHERE type='table';");
@@ -48,11 +42,46 @@ class DbBackedSlashingProtectionFactoryTest {
     final List<String> tableNames = Lists.newArrayList();
     while (rs.next()) {
       tableNames.add(rs.getString(1));
-
     }
 
     assertThat(tableNames).containsOnly("flyway_schema_history", "validators", "signed_blocks",
         "signed_attestations");
   }
 
+  @Test
+  void migratingADatabaseDoesNotImpactItsContent(@TempDir Path testDir) throws SQLException {
+    final String jdbcUrl = createSqliteDatabase(testDir);
+    final Connection conn = DriverManager.getConnection(jdbcUrl);
+    final ComboPooledDataSource dataSource = new ComboPooledDataSource();
+    dataSource.setJdbcUrl(jdbcUrl);
+
+    //insert data
+    final int rowsInserted =
+        conn.createStatement()
+            .executeUpdate("INSERT INTO validators(id, public_key) VALUES (1, \"abcd\")");
+
+    assertThat(rowsInserted).isOne();
+
+    //attempt to create database again
+    createSqliteDatabase(testDir);
+
+    final Table validatorTable = new Table(dataSource, "validators");
+
+    org.assertj.db.api.Assertions.assertThat(validatorTable).hasNumberOfRows(1);
+    org.assertj.db.api.Assertions.assertThat(validatorTable).column("id").containsValues(1);
+    org.assertj.db.api.Assertions.assertThat(validatorTable).column("public_key")
+        .containsValues("abcd");
+  }
+
+  // Returns the JDBC URL of the database
+  private String createSqliteDatabase(final Path rootDir) {
+    final Path dbPath = rootDir.toAbsolutePath().resolve("test.db");
+
+    final String sqlLitePath = "jdbc:sqlite:" + dbPath.toString();
+
+    DbBackedSlashingProtectionFactory.createDbBackedSlashingProtection(sqlLitePath, null, null);
+    assertThat(dbPath.toFile().exists()).isTrue();
+
+    return sqlLitePath;
+  }
 }
