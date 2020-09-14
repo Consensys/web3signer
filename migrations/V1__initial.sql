363c3f12a585449d710df1fa052abb982c9645ce
Trent Mohay <trent.mohay@consensys.net>
2020-09-14 13:18:40 +1000

    Include migrations in distribution

---
+++ migrations/V1__initial.sql
@@ -0,0 +1,19 @@
+CREATE TABLE validators (
+  id SERIAL PRIMARY KEY,
+  public_key BYTEA NOT NULL
+);
+CREATE TABLE signed_blocks (
+    validator_id INTEGER NOT NULL,
+    slot INTEGER NOT NULL,
+    signing_root BYTEA NOT NULL,
+    FOREIGN KEY(validator_id) REFERENCES validators(id),
+    UNIQUE (validator_id, slot)
+);
+CREATE TABLE signed_attestations (
+    validator_id INTEGER,
+    source_epoch INTEGER NOT NULL,
+    target_epoch INTEGER NOT NULL,
+    signing_root BYTEA NOT NULL,
+    FOREIGN KEY(validator_id) REFERENCES validators(id),
+    UNIQUE (validator_id, target_epoch)
+);
