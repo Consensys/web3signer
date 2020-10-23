72c2ac6f64874eda71f5566e3590f6976bfec142
Jason Frame <909467+jframe@users.noreply.github.com>
2020-10-23 15:06:23 +1000

    remove unused variable

--- acceptance-tests/src/test/java/tech/pegasys/web3signer/dsl/utils/Eth2RequestUtils.java
+++ acceptance-tests/src/test/java/tech/pegasys/web3signer/dsl/utils/Eth2RequestUtils.java
@@ -168,7 +168,6 @@ public class Eth2RequestUtils {
   }
 
   private static Eth2SigningRequestBody createDepositRequest() {
-    final ForkInfo forkInfo = forkInfo();
     final DepositMessage depositMessage =
         new DepositMessage(
             BLSPubKey.fromHexString(
