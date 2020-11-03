64f9af6257d9b40c1d7402b762d3453b91dcc0c9
Usman Saleem <usman@usmans.info>
2020-11-03 16:54:41 +1000

    Add SECP acceptance test
    Signed-off-by: Usman Saleem <usman@usmans.info>

--- acceptance-tests/src/test/java/tech/pegasys/web3signer/tests/publickeys/YubiHsmKeysAcceptanceTest.java
+++ acceptance-tests/src/test/java/tech/pegasys/web3signer/tests/publickeys/YubiHsmKeysAcceptanceTest.java
@@ -12,8 +12,11 @@
  */
 package tech.pegasys.web3signer.tests.publickeys;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.containsInAnyOrder;
+import static tech.pegasys.web3signer.core.util.IdentifierUtils.normaliseIdentifier;
 
+import tech.pegasys.signers.secp256k1.EthPublicKeyUtils;
 import tech.pegasys.teku.bls.BLSSecretKey;
 import tech.pegasys.web3signer.core.signing.KeyType;
 import tech.pegasys.web3signer.dsl.utils.MetadataFileHelpers;
@@ -26,6 +29,7 @@ import org.apache.tuweni.bytes.Bytes32;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.web3j.crypto.Credentials;
 
 @Disabled("Requires physical access to YubiHSM device and SDK installed")
 public class YubiHsmKeysAcceptanceTest extends KeyIdentifiersAcceptanceTestBase {
@@ -35,7 +39,7 @@ public class YubiHsmKeysAcceptanceTest extends KeyIdentifiersAcceptanceTestBase 
 
   // following keys are expected to be pre-loaded in YubiHSM as Opaque Data (HEX informat). Opaque
   // Id 1..N.
-  private static final List<String> PRE_LOADED_PRIVATE_KEYS =
+  private static final List<String> PRE_LOADED_BLS_PRIVATE_KEYS =
       List.of(
           "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f",
           "73d51abbd89cb8196f0efb6892f94d68fccc2c35f0b84609e5f12c55dd85aba8",
@@ -49,6 +53,14 @@ public class YubiHsmKeysAcceptanceTest extends KeyIdentifiersAcceptanceTestBase 
           "0bdeebbad8f9b240192635c42f40f2d02ee524c5a3fe8cda53fb4897b08c66fe",
           "5e8d5667ce78982a07242739ab03dc63c91e830c80a5b6adca777e3f216a405d");
 
+  // following SECP keys are expected to be pre-loaded in YubiHSM as Opaque Data (HEX informat).
+  // Opaque Id 20, 21, 22.
+  private static final List<String> PRE_LOADED_SECP_PRIVATE_KEYS =
+      List.of(
+          "ee0b33ddc9d584a567b0df55e88550966f6b830cb9dff6334c01d416bc129ef3",
+          "c19744babeaf9fb0fdbb0cc0076472f5ce3f23551d5c2cd0145b8e341135156c",
+          "188523561ef3688b43a96ecb110583a737e6067194a5c9f5570caa9c122ad8ef");
+
   // Using usb connector
   private static final String CONNECTOR_URL = "yhusb://";
   private static final String ADDITIONAL_INIT_CONFIG = "debug libdebug";
@@ -59,11 +71,27 @@ public class YubiHsmKeysAcceptanceTest extends KeyIdentifiersAcceptanceTestBase 
   private static final MetadataFileHelpers METADATA_FILE_HELPERS = new MetadataFileHelpers();
 
   @BeforeEach
-  void init() {
+  void checkPkcs11ModuleExist() {
+    assertThat(Path.of(MODULE_PATH)).exists();
+  }
 
-    // create meta configuration files
+  @Test
+  public void blsKeysAreLoadedFromYubiHsm() {
+    createBlsConfigurationFiles();
+
+    initAndStartSigner(calculateMode(KeyType.BLS));
+
+    final Response response = signer.callApiPublicKeys(KeyType.BLS);
+    final String[] expectedPublicKeys =
+        PRE_LOADED_BLS_PRIVATE_KEYS.stream()
+            .map(key -> BLSSecretKey.fromBytes(Bytes32.fromHexString(key)).toPublicKey().toString())
+            .toArray(String[]::new);
+    validateApiResponse(response, containsInAnyOrder(expectedPublicKeys));
+  }
+
+  private void createBlsConfigurationFiles() {
     for (short opaqueDataId = (short) 1;
-        opaqueDataId <= PRE_LOADED_PRIVATE_KEYS.size();
+        opaqueDataId <= PRE_LOADED_BLS_PRIVATE_KEYS.size();
         opaqueDataId++) {
       final Path configFile = testDirectory.resolve("yubihsm_" + opaqueDataId + ".yaml");
       METADATA_FILE_HELPERS.createYubihsmYamlFileAt(
@@ -76,17 +104,42 @@ public class YubiHsmKeysAcceptanceTest extends KeyIdentifiersAcceptanceTestBase 
           opaqueDataId,
           KeyType.BLS);
     }
-
-    initAndStartSigner(calculateMode(KeyType.BLS));
   }
 
   @Test
-  public void blsKeysAreLoadedFromYubiHsm() {
-    final Response response = signer.callApiPublicKeys(KeyType.BLS);
+  public void secpKeysAreLoadedFromYubiHsm() {
+    createSecpConfigurationFiles();
+
+    initAndStartSigner(calculateMode(KeyType.SECP256K1));
+
+    final Response response = signer.callApiPublicKeys(KeyType.SECP256K1);
+
     final String[] expectedPublicKeys =
-        PRE_LOADED_PRIVATE_KEYS.stream()
-            .map(key -> BLSSecretKey.fromBytes(Bytes32.fromHexString(key)).toPublicKey().toString())
-            .toArray(String[]::new);
+        PRE_LOADED_SECP_PRIVATE_KEYS.stream().map(this::getPublicKey).toArray(String[]::new);
     validateApiResponse(response, containsInAnyOrder(expectedPublicKeys));
+  }
+
+  private void createSecpConfigurationFiles() {
+    for (short opaqueDataId = 20;
+        opaqueDataId < PRE_LOADED_SECP_PRIVATE_KEYS.size() + 20;
+        opaqueDataId++) {
+      final Path configFile = testDirectory.resolve("yubihsm_" + opaqueDataId + ".yaml");
+      METADATA_FILE_HELPERS.createYubihsmYamlFileAt(
+          configFile,
+          MODULE_PATH,
+          CONNECTOR_URL,
+          ADDITIONAL_INIT_CONFIG,
+          AUTH_ID,
+          PASSWORD,
+          opaqueDataId,
+          KeyType.SECP256K1);
+    }
+  }
+
+  private String getPublicKey(final String key) {
+    return normaliseIdentifier(
+        EthPublicKeyUtils.toHexString(
+            EthPublicKeyUtils.createPublicKey(
+                Credentials.create(key).getEcKeyPair().getPublicKey())));
   }
 }
