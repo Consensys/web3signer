1b58e76166c182d93979efac10b846157e89907d
Jason Frame <909467+jframe@users.noreply.github.com>
2020-11-06 14:22:49 +1000

    Use Medalla GVR in ATs

--- acceptance-tests/src/test/java/tech/pegasys/web3signer/dsl/utils/Eth2RequestUtils.java
+++ acceptance-tests/src/test/java/tech/pegasys/web3signer/dsl/utils/Eth2RequestUtils.java
@@ -264,7 +264,7 @@ public class Eth2RequestUtils {
             Bytes4.fromHexString("0x00000001"),
             UInt64.valueOf(1));
     final Bytes32 genesisValidatorsRoot =
-        Bytes32.fromHexString("0x270d43e74ce340de4bca2b1936beca0f4f5408d9e78aec4850920baf659d5b69");
+        Bytes32.fromHexString("0x04700007fabc8282644aed6d1c7c9e21d38a03a0c4ba193f3afe428824b3a673");
     return new ForkInfo(fork, genesisValidatorsRoot);
   }
 }
