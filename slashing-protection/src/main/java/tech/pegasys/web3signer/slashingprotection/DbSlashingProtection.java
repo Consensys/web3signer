d42b6885c11e58726874f5e0320ef0f7f92bdde7
Trent Mohay <trent.mohay@consensys.net>
2020-11-10 14:21:34 +1100

    rollback signingrecords idea

--- slashing-protection/src/main/java/tech/pegasys/web3signer/slashingprotection/DbSlashingProtection.java
+++ slashing-protection/src/main/java/tech/pegasys/web3signer/slashingprotection/DbSlashingProtection.java
@@ -16,7 +16,6 @@ import static com.fasterxml.jackson.databind.SerializationFeature.FLUSH_AFTER_WR
 import static org.jdbi.v3.core.transaction.TransactionIsolationLevel.READ_COMMITTED;
 import static org.jdbi.v3.core.transaction.TransactionIsolationLevel.SERIALIZABLE;
 
-import tech.pegasys.web3signer.slashingprotection.dao.SignedAttestation;
 import tech.pegasys.web3signer.slashingprotection.dao.SignedAttestationsDao;
 import tech.pegasys.web3signer.slashingprotection.dao.SignedBlocksDao;
 import tech.pegasys.web3signer.slashingprotection.dao.Validator;
@@ -24,7 +23,7 @@ import tech.pegasys.web3signer.slashingprotection.dao.ValidatorsDao;
 import tech.pegasys.web3signer.slashingprotection.interchange.InterchangeManager;
 import tech.pegasys.web3signer.slashingprotection.interchange.InterchangeModule;
 import tech.pegasys.web3signer.slashingprotection.interchange.InterchangeV5Manager;
-import tech.pegasys.web3signer.slashingprotection.validator.AttestationSigningRecords;
+import tech.pegasys.web3signer.slashingprotection.validator.AttestationValidator;
 import tech.pegasys.web3signer.slashingprotection.validator.BlockValidator;
 
 import java.io.IOException;
@@ -111,29 +110,33 @@ public class DbSlashingProtection implements SlashingProtection {
     return jdbi.inTransaction(
         READ_COMMITTED,
         handle -> {
-          final SignedAttestation receivedAttestation =
-              new SignedAttestation(validatorId, sourceEpoch, targetEpoch, signingRoot);
+          final AttestationValidator attestationValidator =
+              new AttestationValidator(
+                  handle,
+                  publicKey,
+                  signingRoot,
+                  sourceEpoch,
+                  targetEpoch,
+                  validatorId,
+                  signedAttestationsDao);
 
-          final AttestationSigningRecords signingRecords =
-              new AttestationSigningRecords(handle, signedAttestationsDao);
-
-          if (!signingRecords.sourceGreaterThanTargetEpoch(receivedAttestation)) {
+          if (!attestationValidator.sourceGreaterThanTargetEpoch()) {
             return false;
           }
 
           lockForValidator(handle, LockType.ATTESTATION, validatorId);
 
-          if (signingRecords.directlyConflictsWithExistingEntry(receivedAttestation)
-              || signingRecords.isSurroundedByExistingAttestation(receivedAttestation)
-              || signingRecords.surroundsExistingAttestation(receivedAttestation)) {
+          if (attestationValidator.directlyConflictsWithExistingEntry()
+              || attestationValidator.isSurroundedByExistingAttestation()
+              || attestationValidator.surroundsExistingAttestation()) {
             return false;
-          } else if (signingRecords.existsInDatabase(receivedAttestation)) {
+          } else if (attestationValidator.existsInDatabase()) {
             return true;
-          } else if (signingRecords.hasSourceOlderThanWatermark(receivedAttestation)
-              || signingRecords.hasTargetOlderThanWatermark(receivedAttestation)) {
+          } else if (attestationValidator.hasSourceOlderThanWatermark()
+              || attestationValidator.hasTargetOlderThanWatermark()) {
             return false;
           }
-          signingRecords.add(receivedAttestation);
+          attestationValidator.insertToDatabase();
           return true;
         });
   }
