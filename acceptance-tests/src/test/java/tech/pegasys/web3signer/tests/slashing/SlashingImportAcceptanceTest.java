18cc94b40f15ce6818f7bcfc42bf6def4459d011
Jason Frame <909467+jframe@users.noreply.github.com>
2020-11-13 16:40:52 +1000

    use values from parsing interchange file

--- acceptance-tests/src/test/java/tech/pegasys/web3signer/tests/slashing/SlashingImportAcceptanceTest.java
+++ acceptance-tests/src/test/java/tech/pegasys/web3signer/tests/slashing/SlashingImportAcceptanceTest.java
@@ -13,7 +13,6 @@
 package tech.pegasys.web3signer.tests.slashing;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static tech.pegasys.web3signer.dsl.utils.Eth2RequestUtils.GENESIS_VALIDATORS_ROOT;
 import static tech.pegasys.web3signer.dsl.utils.WaitUtils.waitFor;
 
 import tech.pegasys.teku.bls.BLSKeyPair;
@@ -21,16 +20,23 @@ import tech.pegasys.web3signer.core.signing.KeyType;
 import tech.pegasys.web3signer.dsl.signer.Signer;
 import tech.pegasys.web3signer.dsl.signer.SignerConfigurationBuilder;
 import tech.pegasys.web3signer.dsl.utils.MetadataFileHelpers;
+import tech.pegasys.web3signer.slashingprotection.interchange.InterchangeModule;
+import tech.pegasys.web3signer.slashingprotection.interchange.model.SignedAttestation;
+import tech.pegasys.web3signer.slashingprotection.interchange.model.SignedBlock;
 import tech.pegasys.web3signer.tests.AcceptanceTestBase;
 
 import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.net.URISyntaxException;
 import java.nio.file.Path;
 import java.util.List;
 import java.util.Map;
 
+import com.fasterxml.jackson.databind.MapperFeature;
 import com.google.common.io.Resources;
+import dsl.InterchangeV5Format;
+import dsl.SignedArtifacts;
 import org.apache.tuweni.bytes.Bytes;
 import org.jdbi.v3.core.Jdbi;
 import org.junit.jupiter.api.Test;
@@ -41,12 +47,11 @@ public class SlashingImportAcceptanceTest extends AcceptanceTestBase {
   private static final MetadataFileHelpers metadataFileHelpers = new MetadataFileHelpers();
   public static final String DB_USERNAME = "postgres";
   public static final String DB_PASSWORD = "postgres";
-  public static final String VALIDATOR_PUBLIC_KEY =
-      "0x8f3f44b74d316c3293cced0c48c72e021ef8d145d136f2908931090e7181c3b777498128a348d07b0b9cd3921b5ca537";
-  public static final String ATTESTATION_SIGNING_ROOT =
-      "0x30752da173420e64a66f6ca6b97c55a96390a3158a755ecd277812488bb84e57";
-  public static final String BLOCK_SIGNING_ROOT =
-      "0x4ff6f743a43f3b4f95350831aeaf0a122a1a392922c45d804280284a69eb850b";
+
+  private static final com.fasterxml.jackson.databind.ObjectMapper objectMapper =
+      new com.fasterxml.jackson.databind.ObjectMapper()
+          .registerModule(new InterchangeModule())
+          .enable(MapperFeature.ACCEPT_CASE_INSENSITIVE_ENUMS);
 
   protected final BLSKeyPair keyPair = BLSKeyPair.random(0);
 
@@ -68,7 +73,7 @@ public class SlashingImportAcceptanceTest extends AcceptanceTestBase {
   }
 
   @Test
-  void slashingDataIsExported(@TempDir Path testDirectory) throws URISyntaxException {
+  void slashingDataIsExported(@TempDir Path testDirectory) throws URISyntaxException, IOException {
     setupSigner(testDirectory, true);
 
     final Path importFile =
@@ -88,36 +93,46 @@ public class SlashingImportAcceptanceTest extends AcceptanceTestBase {
     importSigner.start();
     waitFor(() -> assertThat(importSigner.isRunning()).isFalse());
 
+    final InterchangeV5Format interchangeData =
+        objectMapper.readValue(importFile.toFile(), InterchangeV5Format.class);
+
     final Jdbi jdbi = Jdbi.create(signer.getSlashingDbUrl(), DB_USERNAME, DB_PASSWORD);
+    final int validatorId = 1;
+
+    final Map<String, Object> metadata =
+        jdbi.withHandle(h -> h.select("SELECT * from metadata").mapToMap().one());
+    assertThat(metadata.get("id")).isEqualTo(validatorId);
+    assertThat(metadata.get("genesis_validators_root"))
+        .isEqualTo(interchangeData.getMetadata().getGenesisValidatorsRoot().toArray());
+
+    final SignedArtifacts artifacts = interchangeData.getSignedArtifacts().get(0);
 
     final List<Map<String, Object>> validators =
         jdbi.withHandle(h -> h.select("SELECT * from validators").mapToMap().list());
     assertThat(validators).hasSize(1);
-    assertThat(validators.get(0).get("id")).isEqualTo(1);
+    assertThat(validators.get(0).get("id")).isEqualTo(validatorId);
     assertThat(validators.get(0).get("public_key"))
-        .isEqualTo(Bytes.fromHexString(VALIDATOR_PUBLIC_KEY).toArray());
-
-    final Map<String, Object> metadata =
-        jdbi.withHandle(h -> h.select("SELECT * from metadata").mapToMap().one());
-    assertThat(metadata.get("id")).isEqualTo(1);
-    assertThat(metadata.get("genesis_validators_root"))
-        .isEqualTo(Bytes.fromHexString(GENESIS_VALIDATORS_ROOT).toArray());
+        .isEqualTo(Bytes.fromHexString(artifacts.getPublicKey()).toArray());
 
     final List<Map<String, Object>> signedAttestations =
         jdbi.withHandle(h -> h.select("SELECT * from signed_attestations").mapToMap().list());
+    final SignedAttestation attestation = artifacts.getSignedAttestations().get(0);
     assertThat(signedAttestations).hasSize(1);
-    assertThat(signedAttestations.get(0).get("validator_id")).isEqualTo(1);
-    assertThat(signedAttestations.get(0).get("source_epoch")).isEqualTo(BigDecimal.valueOf(5));
-    assertThat(signedAttestations.get(0).get("target_epoch")).isEqualTo(BigDecimal.valueOf(6));
+    assertThat(signedAttestations.get(0).get("validator_id")).isEqualTo(validatorId);
+    assertThat(signedAttestations.get(0).get("source_epoch"))
+        .isEqualTo(BigDecimal.valueOf(attestation.getSourceEpoch().toLong()));
+    assertThat(signedAttestations.get(0).get("target_epoch"))
+        .isEqualTo(BigDecimal.valueOf(attestation.getTargetEpoch().toLong()));
     assertThat(signedAttestations.get(0).get("signing_root"))
-        .isEqualTo(Bytes.fromHexString(ATTESTATION_SIGNING_ROOT).toArray());
+        .isEqualTo(attestation.getSigningRoot().toArray());
 
     final List<Map<String, Object>> signedBlocks =
         jdbi.withHandle(h -> h.select("SELECT * from signed_blocks").mapToMap().list());
+    final SignedBlock block = artifacts.getSignedBlocks().get(0);
     assertThat(signedBlocks).hasSize(1);
-    assertThat(signedBlocks.get(0).get("validator_id")).isEqualTo(1);
-    assertThat(signedBlocks.get(0).get("slot")).isEqualTo(BigDecimal.valueOf(12345));
-    assertThat(signedBlocks.get(0).get("signing_root"))
-        .isEqualTo(Bytes.fromHexString(BLOCK_SIGNING_ROOT).toArray());
+    assertThat(signedBlocks.get(0).get("validator_id")).isEqualTo(validatorId);
+    assertThat(signedBlocks.get(0).get("slot"))
+        .isEqualTo(BigDecimal.valueOf(block.getSlot().toLong()));
+    assertThat(signedBlocks.get(0).get("signing_root")).isEqualTo(block.getSigningRoot().toArray());
   }
 }
