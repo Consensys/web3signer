2f74456d7d59cb0d7a6b3920cb9e1f692fe02a55
Jason Frame <909467+jframe@users.noreply.github.com>
2020-11-25 13:46:13 +1000

    move retry logic into own class

--- slashing-protection/src/main/java/tech/pegasys/web3signer/slashingprotection/validator/GenesisValidatorRootValidator.java
+++ slashing-protection/src/main/java/tech/pegasys/web3signer/slashingprotection/validator/GenesisValidatorRootValidator.java
@@ -12,38 +12,34 @@
  */
 package tech.pegasys.web3signer.slashingprotection.validator;
 
+import static org.jdbi.v3.core.transaction.TransactionIsolationLevel.READ_COMMITTED;
+
+import tech.pegasys.web3signer.slashingprotection.DbTransactionRetryer;
 import tech.pegasys.web3signer.slashingprotection.dao.MetadataDao;
 
 import java.util.Optional;
-import java.util.Random;
-import java.util.concurrent.Callable;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.tuweni.bytes.Bytes32;
 import org.jdbi.v3.core.Jdbi;
-import org.jdbi.v3.core.statement.UnableToExecuteStatementException;
 
 public class GenesisValidatorRootValidator {
 
   private static final Logger LOG = LogManager.getLogger();
+  private static final int MAX_RETRIES = 3;
   private static final int RETRY_MS = 50;
-
-  private final Jdbi jbdi;
   private final MetadataDao metadataDao;
-  private final Random random = new Random();
+  private final DbTransactionRetryer dbTransactionRetrier;
 
   public GenesisValidatorRootValidator(final Jdbi jdbi, final MetadataDao metadataDao) {
-    this.jbdi = jdbi;
     this.metadataDao = metadataDao;
+    this.dbTransactionRetrier = new DbTransactionRetryer(jdbi, MAX_RETRIES, RETRY_MS);
   }
 
   public boolean checkGenesisValidatorsRootAndInsertIfEmpty(Bytes32 genesisValidatorsRoot) {
-    return retry(() -> checkGvr(genesisValidatorsRoot));
-  }
-
-  private boolean checkGvr(final Bytes32 genesisValidatorsRoot) {
-    return jbdi.inTransaction(
+    return dbTransactionRetrier.handleWithTransactionRetry(
+        READ_COMMITTED,
         handle -> {
           final Optional<Bytes32> dbGvr = metadataDao.findGenesisValidatorsRoot(handle);
           final boolean isValidGvr =
@@ -57,28 +53,5 @@ public class GenesisValidatorRootValidator {
           }
           return isValidGvr;
         });
-  }
-
-  private <V> V retry(final Callable<V> callable) {
-    try {
-      return callable.call();
-    } catch (Exception e) {
-      if (e instanceof UnableToExecuteStatementException) {
-        LOG.debug("Unable to insert GVR due to contention on metadata table. Retrying GVR check");
-        try {
-          final int jitter = random.nextInt(50);
-          Thread.sleep(RETRY_MS + jitter);
-        } catch (InterruptedException ie) {
-          throw new IllegalStateException(ie);
-        }
-        try {
-          return callable.call();
-        } catch (Exception exception) {
-          throw new IllegalStateException(e);
-        }
-      } else {
-        throw new IllegalStateException(e);
-      }
-    }
   }
 }
