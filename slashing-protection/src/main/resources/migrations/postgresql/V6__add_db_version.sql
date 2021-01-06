126a3382ae78ebc0c1bff4a76f11a0e71c9f8263
Trent Mohay <trent.mohay@consensys.net>
2021-01-06 11:44:49 +1100

    move migration to v7

--- slashing-protection/src/main/resources/migrations/postgresql/V6__add_db_version.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE database_version (
-    id INTEGER PRIMARY KEY,
-    version INTEGER NOT NULL
-);
--- Start at version 6, should have previously existed (but now represents migration index).
-INSERT INTO database_version (id, version) VALUES (1, 6);
