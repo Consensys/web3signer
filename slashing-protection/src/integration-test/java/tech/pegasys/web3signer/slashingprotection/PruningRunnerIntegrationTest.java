10188a46055e8fe68d69ffe1d8994bc19cf062b5
Jason Frame <jason.frame@consensys.net>
2021-03-19 13:56:02 +1000

    cleanup pruning runner IT

--- slashing-protection/src/integration-test/java/tech/pegasys/web3signer/slashingprotection/PruningRunnerIntegrationTest.java
+++ slashing-protection/src/integration-test/java/tech/pegasys/web3signer/slashingprotection/PruningRunnerIntegrationTest.java
@@ -22,7 +22,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledThreadPoolExecutor;
-import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -32,51 +31,47 @@ import org.apache.tuweni.bytes.Bytes;
 import org.apache.tuweni.bytes.Bytes32;
 import org.apache.tuweni.units.bigints.UInt64;
 import org.awaitility.Awaitility;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class PruningRunnerIntegrationTest extends IntegrationTestBase {
 
   public static final String PRUNING_THREAD_NAME = "slashing-db-pruner-0";
+  private ScheduledExecutorService scheduledExecutorService;
+  private TestSlashingProtectionParameters slashingProtectionParameters;
+  private SlashingProtection slashingProtection;
+
+  @BeforeEach
+  void setupSlashingProtection() {
+    slashingProtectionParameters =
+        new TestSlashingProtectionParameters(databaseUrl, USERNAME, PASSWORD, 5, 1);
+    scheduledExecutorService =
+        new ScheduledThreadPoolExecutor(
+            1, new ThreadFactoryBuilder().setNameFormat("slashing-db-pruner" + "-%d").build());
+    slashingProtection =
+        SlashingProtectionFactory.createSlashingProtection(slashingProtectionParameters);
+    insertValidatorAndCreateSlashingData(slashingProtection, 10, 10, 1);
+  }
 
   @Test
   void prunesValidatorsForExecuteOnOwnThread() {
-    final TestSlashingProtectionParameters slashingProtectionParameters =
-        new TestSlashingProtectionParameters(databaseUrl, USERNAME, PASSWORD, 5, 1);
-    final SlashingProtection slashingProtection =
-        SlashingProtectionFactory.createSlashingProtection(slashingProtectionParameters);
     final TestSlashingProtection testSlashingProtection =
         new TestSlashingProtection(slashingProtection);
-    slashingProtection.registerValidators(List.of(Bytes.of(1)));
-    final ThreadFactory threadFactory =
-        new ThreadFactoryBuilder().setNameFormat("slashing-db-pruner" + "-%d").build();
-    final ScheduledExecutorService scheduledExecutorService =
-        new ScheduledThreadPoolExecutor(1, threadFactory);
     final DbPrunerRunner dbPrunerRunner =
         new DbPrunerRunner(
             slashingProtectionParameters, testSlashingProtection, scheduledExecutorService);
-    insertAndRegisterData(slashingProtection, 10, 10, 1);
 
     dbPrunerRunner.execute();
-    // wait for pruning to finish
-    Awaitility.await()
-        .ignoreExceptions()
-        .atMost(30, TimeUnit.SECONDS)
-        .untilAsserted(() -> assertThat(testSlashingProtection.getPruningStats()).hasSize(1));
 
-    assertThat(testSlashingProtection.getPruningStats()).hasSize(1);
-    assertThat(testSlashingProtection.getPruningStats().get(0).getThread().getName())
-        .isEqualTo(PRUNING_THREAD_NAME);
+    final List<PruningStat> pruningStats = testSlashingProtection.getPruningStats();
+    waitForPruningToFinish(pruningStats, 1);
+    assertThat(pruningStats.get(0).getThread().getName()).isEqualTo(PRUNING_THREAD_NAME);
     assertThat(fetchAttestations(1)).hasSize(5);
     assertThat(fetchBlocks(1)).hasSize(5);
   }
 
   @Test
   void prunesValidatorsForExecuteHandlesErrors() {
-    final TestSlashingProtectionParameters slashingProtectionParameters =
-        new TestSlashingProtectionParameters(databaseUrl, USERNAME, PASSWORD, 5, 1);
-    final SlashingProtection slashingProtection =
-        SlashingProtectionFactory.createSlashingProtection(slashingProtectionParameters);
-
     final AtomicInteger pruningCount = new AtomicInteger(0);
     final TestSlashingProtection testSlashingProtection =
         new TestSlashingProtection(
@@ -88,72 +83,44 @@ public class PruningRunnerIntegrationTest extends IntegrationTestBase {
                 slashingProtection.prune();
               }
             });
-
-    slashingProtection.registerValidators(List.of(Bytes.of(1)));
-    final ThreadFactory threadFactory =
-        new ThreadFactoryBuilder().setNameFormat("slashing-db-pruner" + "-%d").build();
-    final ScheduledExecutorService scheduledExecutorService =
-        new ScheduledThreadPoolExecutor(1, threadFactory);
     final DbPrunerRunner dbPrunerRunner =
         new DbPrunerRunner(
             slashingProtectionParameters, testSlashingProtection, scheduledExecutorService);
-    insertAndRegisterData(slashingProtection, 10, 10, 1);
 
+    // run expecting error to occur and no pruning to happen
     dbPrunerRunner.execute();
-    // wait for pruning to finish
-    Awaitility.await()
-        .ignoreExceptions()
-        .atMost(30, TimeUnit.SECONDS)
-        .untilAsserted(() -> assertThat(testSlashingProtection.getPruningStats()).isNotEmpty());
 
+    final List<PruningStat> pruningStats = testSlashingProtection.getPruningStats();
+    waitForPruningToFinish(pruningStats, 1);
     final Set<Thread> currentThreads = Thread.getAllStackTraces().keySet();
-    assertThat(currentThreads.stream().anyMatch(t -> t.getName().equals("slashing-db-pruner-0")))
+    assertThat(currentThreads.stream().anyMatch(t -> t.getName().equals(PRUNING_THREAD_NAME)))
         .isTrue();
     assertThat(fetchAttestations(1)).hasSize(10);
     assertThat(fetchBlocks(1)).hasSize(10);
 
     // ensure that pruning still works after the failed execution
-    testSlashingProtection.resetPruningStats();
     dbPrunerRunner.execute();
-    // wait for pruning to finish
-    Awaitility.await()
-        .ignoreExceptions()
-        .atMost(30, TimeUnit.SECONDS)
-        .untilAsserted(() -> assertThat(testSlashingProtection.getPruningStats()).isNotEmpty());
-    assertThat(testSlashingProtection.getPruningStats()).hasSize(1);
-    assertThat(testSlashingProtection.getPruningStats().get(0).getThread().getName())
-        .isEqualTo(PRUNING_THREAD_NAME);
+
+    waitForPruningToFinish(pruningStats, 2);
+    assertThat(pruningStats.get(1).getThread().getName()).isEqualTo(PRUNING_THREAD_NAME);
     assertThat(fetchAttestations(1)).hasSize(5);
     assertThat(fetchBlocks(1)).hasSize(5);
   }
 
   @Test
   void prunesValidatorsForScheduledRunAreRunPeriodically() {
-    final TestSlashingProtectionParameters slashingProtectionParameters =
+    final SlashingProtectionParameters slashingProtectionParameters =
         new TestSlashingProtectionParameters(databaseUrl, USERNAME, PASSWORD, 5, 1, 1);
-    final SlashingProtection slashingProtection =
-        SlashingProtectionFactory.createSlashingProtection(slashingProtectionParameters);
     final TestSlashingProtection testSlashingProtection =
         new TestSlashingProtection(slashingProtection);
-    slashingProtection.registerValidators(List.of(Bytes.of(1)));
-    final ThreadFactory threadFactory =
-        new ThreadFactoryBuilder().setNameFormat("slashing-db-pruner" + "-%d").build();
-    final ScheduledExecutorService scheduledExecutorService =
-        new ScheduledThreadPoolExecutor(1, threadFactory);
     final DbPrunerRunner dbPrunerRunner =
         new DbPrunerRunner(
             slashingProtectionParameters, testSlashingProtection, scheduledExecutorService);
-    insertAndRegisterData(slashingProtection, 10, 10, 1);
 
     dbPrunerRunner.schedule();
-    final List<PruningStat> pruningStats = testSlashingProtection.getPruningStats();
-    // wait for pruning to finish
-    Awaitility.await()
-        .ignoreExceptions()
-        .atMost(30, TimeUnit.SECONDS)
-        .untilAsserted(() -> assertThat(pruningStats).hasSize(3));
 
-    assertThat(pruningStats).hasSize(3);
+    final List<PruningStat> pruningStats = testSlashingProtection.getPruningStats();
+    waitForPruningToFinish(pruningStats, 3);
     assertThat(
             pruningStats.stream()
                 .allMatch(ps -> ps.getThread().getName().equals(PRUNING_THREAD_NAME)))
@@ -167,29 +134,11 @@ public class PruningRunnerIntegrationTest extends IntegrationTestBase {
     assertThat(fetchBlocks(1)).hasSize(5);
   }
 
-  private void insertAndRegisterData(
-      final SlashingProtection slashingProtection,
-      final int noOfBlocks,
-      final int noOfAttestations,
-      final int validatorId) {
-    final Bytes validatorPublicKey = Bytes.of(validatorId);
-    slashingProtection.registerValidators(List.of(validatorPublicKey));
-    insertData(noOfBlocks, noOfAttestations, validatorId);
-  }
-
-  private void insertData(final int noOfBlocks, final int noOfAttestations, final int validatorId) {
-    for (int b = 0; b < noOfBlocks; b++) {
-      insertBlockAt(UInt64.valueOf(b), validatorId);
-    }
-    for (int a = 0; a < noOfAttestations; a++) {
-      insertAttestationAt(UInt64.valueOf(a), UInt64.valueOf(a), validatorId);
-    }
-
-    jdbi.useTransaction(
-        h -> {
-          lowWatermarkDao.updateSlotWatermarkFor(h, validatorId, UInt64.ZERO);
-          lowWatermarkDao.updateEpochWatermarksFor(h, validatorId, UInt64.ZERO, UInt64.ZERO);
-        });
+  private void waitForPruningToFinish(final List<PruningStat> pruningStats, final int pruningSize) {
+    Awaitility.await()
+        .ignoreExceptions()
+        .atMost(30, TimeUnit.SECONDS)
+        .untilAsserted(() -> assertThat(pruningStats).hasSize(pruningSize));
   }
 
   // Test version of slashing protection that delegates everything but also captures the thread that
@@ -253,10 +202,6 @@ public class PruningRunnerIntegrationTest extends IntegrationTestBase {
 
     public List<PruningStat> getPruningStats() {
       return pruningStats;
-    }
-
-    public void resetPruningStats() {
-      pruningStats.clear();
     }
   }
 
