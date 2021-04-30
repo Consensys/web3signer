0795ef2ac8cc9cd7d995e5585be5db3857c7a516
Usman Saleem <usman@usmans.info>
2021-04-30 11:17:07 +1000

    removing unnecessary import
    Signed-off-by: Usman Saleem <usman@usmans.info>

--- core/src/main/java/tech/pegasys/web3signer/core/Eth2Runner.java
+++ core/src/main/java/tech/pegasys/web3signer/core/Eth2Runner.java
@@ -15,7 +15,6 @@ package tech.pegasys.web3signer.core;
 import static tech.pegasys.web3signer.core.service.http.OpenApiOperationsId.ETH2_LIST;
 import static tech.pegasys.web3signer.core.service.http.OpenApiOperationsId.ETH2_SIGN;
 import static tech.pegasys.web3signer.core.service.http.OpenApiOperationsId.RELOAD;
-import static tech.pegasys.web3signer.core.service.http.metrics.HttpApiMetrics.incSignerLoadCount;
 import static tech.pegasys.web3signer.core.signing.KeyType.BLS;
 
 import tech.pegasys.signers.azure.AzureKeyVault;
