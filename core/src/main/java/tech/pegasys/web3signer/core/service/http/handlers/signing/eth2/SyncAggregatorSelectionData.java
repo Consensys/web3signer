03149f47f1b9279439ef596dddacd64aedce24ca
Usman Saleem <usman@usmans.info>
2021-07-06 14:02:42 +1000

    remove unused code
    Signed-off-by: Usman Saleem <usman@usmans.info>

--- core/src/main/java/tech/pegasys/web3signer/core/service/http/handlers/signing/eth2/SyncAggregatorSelectionData.java
+++ core/src/main/java/tech/pegasys/web3signer/core/service/http/handlers/signing/eth2/SyncAggregatorSelectionData.java
@@ -19,7 +19,6 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import org.apache.tuweni.bytes.Bytes32;
 
 public class SyncAggregatorSelectionData {
-  private Bytes32 beaconBlockRoot;
   private final UInt64 slot;
   private final UInt64 subcommitteeIndex;
 
