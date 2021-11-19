2bf68fd11fb2e986f0b0cb88daff3e8c444b30f4
Usman Saleem <usman@usmans.info>
2021-11-19 11:10:02 +1000

    Update .openapidoc nodejs project to publish specs using version as directory name
    Signed-off-by: Usman Saleem <usman@usmans.info>

--- .openapidoc/config.js
+++ .openapidoc/config.js
@@ -3,14 +3,17 @@ const path = require("path");
 const yaml = require("js-yaml");
 const GitUrlParse = require("git-url-parse");
 
-const distDir = process.env.OA_DIST_DIR || "./dist";
-const specDir =
-  process.env.OA_SPEC_DIR || "../core/build/resources/main/openapi";
+// modify following using env variables in CI environment
 const gitUrl =
   process.env.OA_GIT_URL || "git@github.com:ConsenSys/web3signer.git";
 const gitUserName = process.env.OA_GIT_USERNAME || "CircleCI Build";
 const gitEmail = process.env.OA_GIT_EMAIL || "ci-build@consensys.net";
 const branch = process.env.OA_GH_PAGES_BRANCH || "gh-pages";
+
+// modify following with caution
+const distDir = process.env.OA_DIST_DIR || "./dist";
+const specDir =
+  process.env.OA_SPEC_DIR || "../core/build/publish";
 const versionsFileName = process.env.OA_VERSIONS_FILE_NAME || "versions.json";
 
 module.exports = {
@@ -19,15 +22,13 @@ module.exports = {
 
 function getConfig() {
   const repo = GitUrlParse(gitUrl);
-  const specs = calculateSpecs();
-  if (specs.length == 0) {
-    throw new Error("Unable to parse specs in dist" + distDir);
-  }
-
+  const versionDetails = calculateVersionDetails();
+  const versionJsonDetails = calculateVersionJsonDetails(repo, branch);
   return {
-    specs: specs,
+    specDir: specDir,
     distDir: distDir,
-    versions: calculateVersionDetails(repo, branch),
+    versionDetails: versionDetails,
+    versionJsonDetails: versionJsonDetails,
     ghPagesConfig: {
       add: true, // allows gh-pages module to keep remote files
       branch: branch,
@@ -36,39 +37,23 @@ function getConfig() {
         name: gitUserName,
         email: gitEmail,
       },
-      message: `[skip ci] OpenAPI Publish [${specs[0].version}]`,
+      message: `[skip ci] OpenAPI Publish [${versionDetails.version}]`,
     },
   };
 }
 
-function calculateSpecs() {
-  const extension = ".yaml";
-  const specFiles = fs.readdirSync(specDir);
-  var specArr = [];
-  specFiles.forEach((file) => {
-    if (path.extname(file).toLowerCase() === extension) {
-      specArr.push(calculateSpecDetails(path.join(specDir, file)));
-    }
-  });
-  return specArr;
-}
-
-function calculateSpecDetails(specFile) {
-  const specVersion = calculateSpecVersion(specFile);
+function calculateVersionDetails() {
+  const specFile = specDir + "/web3signer-eth2.yaml"
+  const specVersion = readVersionFromSpecFile(specFile);
   const release = isReleaseVersion(specVersion);
-  const latestDist = destinationPath(specFile, "latest");
-  const releaseDist = destinationPath(specFile, specVersion);
 
   return {
-    path: specFile,
     version: specVersion,
     isReleaseVersion: release,
-    latestDist: latestDist,
-    releaseDist: releaseDist,
   };
 }
 
-function calculateSpecVersion(specFile) {
+function readVersionFromSpecFile(specFile) {
   return yaml.safeLoad(fs.readFileSync(specFile, "utf8")).info.version;
 }
 
@@ -78,13 +63,7 @@ function isReleaseVersion(specVersion) {
   return !specVersion.includes("+");
 }
 
-function destinationPath(specFile, suffix) {
-  const prefix = path.parse(specFile).name;
-  const extension = path.extname(specFile);
-  return path.join(distDir, `${prefix}-${suffix}${extension}`);
-}
-
-function calculateVersionDetails(repo, branch) {
+function calculateVersionJsonDetails(repo, branch) {
   const versionsFileUrl = `https://${repo.source}/${repo.owner}/${repo.name}/raw/${branch}/${versionsFileName}`;
   const versionsFileDist = path.join(distDir, versionsFileName);
   return {
