80b45dfa316909b9a73bcdb48f02ae8065ab5473
Gabriel Fukushima <gabrielfukushima@gmail.com>
2022-07-04 16:27:30 +1000

    Moved classes to tech.pegasys.web3signer.common package
    Mainly to avoid a circular dependency between core and common packages

--- common/src/main/java/tech/pegasys/web3signer/common/ApplicationInfo.java
+++ common/src/main/java/tech/pegasys/web3signer/common/ApplicationInfo.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package tech.pegasys.web3signer.commandline;
+package tech.pegasys.web3signer.common;
 
 public class ApplicationInfo {
   private static final String CLIENT_IDENTITY = "web3signer";
