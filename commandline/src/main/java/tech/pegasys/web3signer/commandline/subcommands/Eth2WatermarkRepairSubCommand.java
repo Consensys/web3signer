37ce2c227a1556760d2fd770c411dcb841119e93
Jason Frame <jason.frame@consensys.net>
2022-11-24 14:13:16 +1000

    public key validation

--- commandline/src/main/java/tech/pegasys/web3signer/commandline/subcommands/Eth2WatermarkRepairSubCommand.java
+++ commandline/src/main/java/tech/pegasys/web3signer/commandline/subcommands/Eth2WatermarkRepairSubCommand.java
@@ -22,6 +22,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.tuweni.bytes.Bytes;
@@ -30,7 +31,10 @@ import org.jdbi.v3.core.Jdbi;
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
 import picocli.CommandLine.HelpCommand;
+import picocli.CommandLine.Model.CommandSpec;
 import picocli.CommandLine.Option;
+import picocli.CommandLine.ParameterException;
+import picocli.CommandLine.Spec;
 
 @Command(
     name = "watermark-repair",
@@ -41,6 +45,8 @@ import picocli.CommandLine.Option;
     mixinStandardHelpOptions = true)
 public class Eth2WatermarkRepairSubCommand implements Runnable {
   private static final Logger LOG = LogManager.getLogger();
+
+  @Spec private CommandSpec commandSpec;
 
   @CommandLine.ParentCommand private Eth2SubCommand eth2Config;
 
@@ -62,7 +68,7 @@ public class Eth2WatermarkRepairSubCommand implements Runnable {
       names = "--validator-ids",
       paramLabel = "<publicKey>",
       description =
-          "List of validators to apply low watermark update to. If none are specified"
+          "List of validator public keys as hexadecimal to apply low watermark update to. If none are specified"
               + " then all validators low watermarks will be updated (default: ${DEFAULT-VALUE}).",
       split = ",",
       arity = "0..*")
@@ -74,8 +80,8 @@ public class Eth2WatermarkRepairSubCommand implements Runnable {
     final ValidatorsDao validatorsDao = new ValidatorsDao();
     final List<Bytes> validatorIdPublicKeys =
         validatorIds.stream()
-            .filter(s -> !s.isEmpty())
-            .map(Bytes::fromHexStringLenient)
+            .filter(StringUtils::isNotEmpty)
+            .map(this::convertToBytes)
             .collect(Collectors.toList());
 
     final SlashingProtectionContext slashingProtectionContext =
@@ -104,5 +110,15 @@ public class Eth2WatermarkRepairSubCommand implements Runnable {
                           h, validator.getId(), UInt64.valueOf(epoch), UInt64.valueOf(epoch));
                     }));
     LOG.info("Updated low watermark for {} validators", validators.size());
+  }
+
+  private Bytes convertToBytes(final String value) {
+    try {
+      return Bytes.fromHexString(value);
+    } catch (IllegalArgumentException e) {
+      throw new ParameterException(
+          commandSpec.commandLine(),
+          "Invalid value for validator public key " + value + ". Value must be in hexadecimal.");
+    }
   }
 }
