17d38a0718261a9281b7661ad88bf152e3f8caf6
Gabriel Fukushima <gabrielfukushima@gmail.com>
2023-05-24 12:46:21 +1000

    Move package
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>

--- acceptance-tests/src/test/java/tech/pegasys/web3signer/tests/eth1rpc/AccountManagementAcceptanceTest.java
+++ acceptance-tests/src/test/java/tech/pegasys/web3signer/tests/eth1rpc/AccountManagementAcceptanceTest.java
@@ -10,7 +10,7 @@
  * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
  * specific language governing permissions and limitations under the License.
  */
-package tech.pegasys.web3signer.tests.account;
+package tech.pegasys.web3signer.tests.eth1rpc;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
