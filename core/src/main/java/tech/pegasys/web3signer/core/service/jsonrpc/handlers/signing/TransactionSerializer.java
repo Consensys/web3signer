931453863edb4c95eba81131bd3f79314664ae0c
Gabriel Fukushima <gabrielfukushima@gmail.com>
2023-06-21 16:07:50 +1000

    add map and throw
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>

--- core/src/main/java/tech/pegasys/web3signer/core/service/jsonrpc/handlers/signing/TransactionSerializer.java
+++ core/src/main/java/tech/pegasys/web3signer/core/service/jsonrpc/handlers/signing/TransactionSerializer.java
@@ -12,18 +12,23 @@
  */
 package tech.pegasys.web3signer.core.service.jsonrpc.handlers.signing;
 
+import static tech.pegasys.web3signer.core.service.jsonrpc.response.JsonRpcError.SIGNING_FROM_IS_NOT_AN_UNLOCKED_ACCOUNT;
+
 import tech.pegasys.signers.secp256k1.api.Signature;
+import tech.pegasys.web3signer.core.service.jsonrpc.exceptions.JsonRpcException;
 import tech.pegasys.web3signer.core.service.jsonrpc.handlers.sendtransaction.transaction.Transaction;
-import tech.pegasys.web3signer.signing.ArtifactSigner;
 import tech.pegasys.web3signer.signing.ArtifactSignerProvider;
 import tech.pegasys.web3signer.signing.SecpArtifactSignature;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.apache.tuweni.bytes.Bytes;
 import org.web3j.crypto.Sign.SignatureData;
 import org.web3j.crypto.TransactionEncoder;
 import org.web3j.utils.Numeric;
 
 public class TransactionSerializer {
+  private static final Logger LOG = LogManager.getLogger();
 
   protected final ArtifactSignerProvider signer;
   protected final long chainId;
@@ -40,9 +45,18 @@ public class TransactionSerializer {
   public String serialize(final Transaction transaction) {
     final byte[] bytesToSign = transaction.rlpEncode(chainId);
 
-    final ArtifactSigner artifactSigner = signer.getSigner(identifier).get();
     final SecpArtifactSignature artifactSignature =
-        ((SecpArtifactSignature) artifactSigner.sign(Bytes.of(bytesToSign)));
+        signer
+            .getSigner(identifier)
+            .map(
+                artifactSigner ->
+                    (SecpArtifactSignature) artifactSigner.sign(Bytes.of(bytesToSign)))
+            .orElseThrow(
+                () -> {
+                  LOG.debug("From address ({}) does not match any available account", identifier);
+                  throw new JsonRpcException(SIGNING_FROM_IS_NOT_AN_UNLOCKED_ACCOUNT);
+                });
+
     final Signature signature = artifactSignature.getSignatureData();
 
     final SignatureData web3jSignature =
