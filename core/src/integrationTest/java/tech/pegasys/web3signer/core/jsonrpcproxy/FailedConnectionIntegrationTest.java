4f6dd05cef0d79f87a39579d7e1bbeb589d753be
Gabriel Fukushima <gabrielfukushima@gmail.com>
2023-07-06 13:43:59 +1000

    Fix proxy integration tests
    Signed-off-by: Gabriel Fukushima <gabrielfukushima@gmail.com>

--- core/src/integrationTest/java/tech/pegasys/web3signer/core/jsonrpcproxy/FailedConnectionIntegrationTest.java
+++ core/src/integrationTest/java/tech/pegasys/web3signer/core/jsonrpcproxy/FailedConnectionIntegrationTest.java
@@ -20,10 +20,10 @@ import io.netty.handler.codec.http.HttpResponseStatus;
 import io.vertx.core.json.Json;
 import org.junit.jupiter.api.Test;
 
-class FailedConnectionIntegrationTest extends IntegrationTestBase {
+class FailedConnectionIntegrationTest extends DefaultTestBase {
 
   @Test
-  void failsToConnectToDownStreamRaisesTimeout() {
+  void failsToConnectToDownStreamRaisesTimeout(){
     clientAndServer.stop();
     final EthProtocolVersionRequest request = new EthProtocolVersionRequest(jsonRpc());
 
