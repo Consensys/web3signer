dc945db62bc77e062566307d1818e3b3f65d500d
Usman Saleem <usman@usmans.info>
2023-07-10 17:09:59 +1000

    aws kms signer factory skeleton

--- signing/src/main/java/tech/pegasys/web3signer/signing/secp256k1/aws/AwsKMSSignerFactory.java
+++ signing/src/main/java/tech/pegasys/web3signer/signing/secp256k1/aws/AwsKMSSignerFactory.java
@@ -1,32 +1,42 @@
+/*
+ * Copyright 2023 ConsenSys AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance with
+ * the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed on
+ * an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
 package tech.pegasys.web3signer.signing.secp256k1.aws;
-
-import com.google.common.base.Preconditions;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import tech.pegasys.web3signer.signing.config.metadata.AwsKMSMetadata;
-import tech.pegasys.web3signer.signing.config.metadata.AwsKeySigningMetadata;
-import tech.pegasys.web3signer.signing.secp256k1.Signer;
 
 import static com.google.common.base.Preconditions.checkArgument;
 
-/**
- * Create AwsKMSSigner and instantiate AWS' KmsClient library.
- */
+import tech.pegasys.web3signer.signing.config.metadata.AwsKMSMetadata;
+import tech.pegasys.web3signer.signing.secp256k1.Signer;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/** Create AwsKMSSigner and instantiate AWS' KmsClient library. */
 public class AwsKMSSignerFactory {
-    private static final Logger LOG = LogManager.getLogger();
-    private final boolean needsToHash; // Apply Hash.sha3(data) before signing
+  private static final Logger LOG = LogManager.getLogger();
+  private final boolean needsToHash; // Apply Hash.sha3(data) before signing
 
-    public AwsKMSSignerFactory() {
-        this(true);
-    }
+  public AwsKMSSignerFactory() {
+    this(true);
+  }
 
-    public AwsKMSSignerFactory(final boolean needsToHash) {
-        this.needsToHash = needsToHash;
-    }
+  public AwsKMSSignerFactory(final boolean needsToHash) {
+    this.needsToHash = needsToHash;
+  }
 
-    public Signer createSigner(final AwsKMSMetadata awsKMSMetadata) {
-        checkArgument(awsKMSMetadata != null, "awsKMSMetadata must not be null");
+  public Signer createSigner(final AwsKMSMetadata awsKMSMetadata) {
+    checkArgument(awsKMSMetadata != null, "awsKMSMetadata must not be null");
 
-
-    }
+    // TODO: Fix ME
+    return new AwsKMSSigner();
+  }
 }
