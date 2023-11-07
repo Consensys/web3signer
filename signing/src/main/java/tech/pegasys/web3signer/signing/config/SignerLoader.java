713ff389e8883b09f168dc4b053d1e785716996c
Usman Saleem <usman@usmans.info>
2023-11-07 15:18:33 +1000

    fix import

--- signing/src/main/java/tech/pegasys/web3signer/signing/config/SignerLoader.java
+++ signing/src/main/java/tech/pegasys/web3signer/signing/config/SignerLoader.java
@@ -38,12 +38,12 @@ import java.util.concurrent.atomic.AtomicLong;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.commons.lang3.time.DurationFormatUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jetbrains.annotations.VisibleForTesting;
 
 public class SignerLoader {
 
