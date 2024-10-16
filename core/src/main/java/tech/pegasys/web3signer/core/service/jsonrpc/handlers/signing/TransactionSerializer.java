a0ddb453c08eda2d104f366e6111e80493478a8e
Nazarii Denha <dengaaa2002@gmail.com>
2024-10-16 09:07:13 +0200

    remove unused

--- core/src/main/java/tech/pegasys/web3signer/core/service/jsonrpc/handlers/signing/TransactionSerializer.java
+++ core/src/main/java/tech/pegasys/web3signer/core/service/jsonrpc/handlers/signing/TransactionSerializer.java
@@ -82,21 +82,9 @@ public class TransactionSerializer {
         .array();
   }
 
-  private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
-  public static String bytesToHex(byte[] bytes) {
-    char[] hexChars = new char[bytes.length * 2];
-    for (int j = 0; j < bytes.length; j++) {
-      int v = bytes[j] & 0xFF;
-      hexChars[j * 2] = HEX_ARRAY[v >>> 4];
-      hexChars[j * 2 + 1] = HEX_ARRAY[v & 0x0F];
-    }
-    return new String(hexChars);
-  }
-
   private byte[] serializeEip4844(final Transaction transaction) {
     byte[] bytesToSign = transaction.rlpEncodeToSign();
     bytesToSign = prependEip4844TransactionType(bytesToSign);
-    System.out.println("signing eip4844 " +bytesToHex(bytesToSign));
 
     final SignatureData signatureData = sign(transaction.sender(), bytesToSign);
 
