a5c3d3104eec88ce29b31f61c415f0fe1def323a
Usman Saleem <usman@usmans.info>
2024-11-27 15:58:51 +1000

    feat: Commit Boost API - Request Signature
     -- Routes and Handlers
     -- Acceptance Test

--- acceptance-tests/src/test/java/tech/pegasys/web3signer/tests/commitboost/CommitBoostSigningRequestAcceptanceTest.java
+++ acceptance-tests/src/test/java/tech/pegasys/web3signer/tests/commitboost/CommitBoostSigningRequestAcceptanceTest.java
@@ -12,10 +12,10 @@
  */
 package tech.pegasys.web3signer.tests.commitboost;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasSize;
 import static tech.pegasys.web3signer.tests.commitboost.CommitBoostGetPubKeysAcceptanceTest.KEYSTORE_PASSWORD;
 import static tech.pegasys.web3signer.tests.commitboost.CommitBoostGetPubKeysAcceptanceTest.createCommitBoostPasswordFile;
+import static tech.pegasys.web3signer.tests.commitboost.CommitBoostGetPubKeysAcceptanceTest.createProxyBLSKeys;
+import static tech.pegasys.web3signer.tests.commitboost.CommitBoostGetPubKeysAcceptanceTest.createProxyECKeys;
 import static tech.pegasys.web3signer.tests.commitboost.CommitBoostGetPubKeysAcceptanceTest.randomBLSKeyPairs;
 
 import tech.pegasys.teku.bls.BLSKeyPair;
@@ -24,29 +24,37 @@ import tech.pegasys.teku.spec.Spec;
 import tech.pegasys.teku.spec.networks.Eth2Network;
 import tech.pegasys.web3signer.KeystoreUtil;
 import tech.pegasys.web3signer.core.service.http.handlers.commitboost.SigningRootGenerator;
-import tech.pegasys.web3signer.core.service.http.handlers.commitboost.json.ProxyDelegation;
-import tech.pegasys.web3signer.core.service.http.handlers.commitboost.json.ProxyKeySignatureScheme;
+import tech.pegasys.web3signer.core.service.http.handlers.commitboost.json.CommitBoostSignRequestType;
 import tech.pegasys.web3signer.dsl.signer.SignerConfigurationBuilder;
 import tech.pegasys.web3signer.dsl.utils.DefaultKeystoresParameters;
 import tech.pegasys.web3signer.dsl.utils.ValidBLSSignatureMatcher;
+import tech.pegasys.web3signer.dsl.utils.ValidK256SignatureMatcher;
 import tech.pegasys.web3signer.signing.config.KeystoresParameters;
+import tech.pegasys.web3signer.signing.secp256k1.EthPublicKeyUtils;
 import tech.pegasys.web3signer.tests.AcceptanceTestBase;
 
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apache.tuweni.bytes.Bytes32;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
+import org.web3j.crypto.ECKeyPair;
 
-public class CommitBoostGenerateProxyKeyAcceptanceTest extends AcceptanceTestBase {
+public class CommitBoostSigningRequestAcceptanceTest extends AcceptanceTestBase {
   private static final SigningRootGenerator SIGNING_ROOT_GENERATOR =
-      new SigningRootGenerator(getSpec(Eth2Network.MAINNET));
+      new SigningRootGenerator(getMainnetSpec());
   private final List<BLSKeyPair> consensusBlsKeys = randomBLSKeyPairs(1);
+  private final Map<String, List<BLSKeyPair>> proxyBLSKeysMap = new HashMap<>();
+  private final Map<String, List<ECKeyPair>> proxySECPKeysMap = new HashMap<>();
 
   @TempDir private Path keystoreDir;
   @TempDir private Path passwordDir;
@@ -59,6 +67,16 @@ public class CommitBoostGenerateProxyKeyAcceptanceTest extends AcceptanceTestBas
     for (final BLSKeyPair blsKeyPair : consensusBlsKeys) {
       // create consensus bls keystore
       KeystoreUtil.createKeystore(blsKeyPair, keystoreDir, passwordDir, KEYSTORE_PASSWORD);
+
+      // create 1 proxy bls
+      final List<BLSKeyPair> proxyBLSKeys =
+          createProxyBLSKeys(blsKeyPair, 1, commitBoostKeystoresPath);
+      proxyBLSKeysMap.put(blsKeyPair.getPublicKey().toHexString(), proxyBLSKeys);
+
+      // create 1 proxy secp keys
+      final List<ECKeyPair> proxyECKeyPairs =
+          createProxyECKeys(blsKeyPair, 1, commitBoostKeystoresPath);
+      proxySECPKeysMap.put(blsKeyPair.getPublicKey().toHexString(), proxyECKeyPairs);
     }
 
     // commit boost proxy keys password file
@@ -80,49 +98,49 @@ public class CommitBoostGenerateProxyKeyAcceptanceTest extends AcceptanceTestBas
     startSigner(configBuilder.build());
   }
 
-  @Test
-  void generateCommitBoostProxyKeys() {
-    for (ProxyKeySignatureScheme scheme : ProxyKeySignatureScheme.values()) {
-      final Response response =
-          signer.callCommitBoostGenerateProxyKey(
-              consensusBlsKeys.get(0).getPublicKey().toHexString(), scheme.name());
+  @ParameterizedTest
+  @EnumSource(CommitBoostSignRequestType.class)
+  void requestCommitBoostSignature(final CommitBoostSignRequestType signRequestType) {
+    final String consensusPubKey =
+        consensusBlsKeys.stream().findFirst().orElseThrow().getPublicKey().toHexString();
+    final String pubKey =
+        switch (signRequestType) {
+          case CONSENSUS -> consensusPubKey;
+          case PROXY_BLS ->
+              proxyBLSKeysMap.get(consensusPubKey).stream()
+                  .findFirst()
+                  .orElseThrow()
+                  .getPublicKey()
+                  .toHexString();
+          case PROXY_ECDSA ->
+              EthPublicKeyUtils.toHexStringCompressed(
+                  EthPublicKeyUtils.web3JPublicKeyToECPublicKey(
+                      proxySECPKeysMap.get(consensusPubKey).stream()
+                          .findFirst()
+                          .orElseThrow()
+                          .getPublicKey()));
+        };
 
-      // verify we got new proxy public key and a valid bls signature
-      response
-          .then()
-          .statusCode(200)
-          .contentType(ContentType.JSON)
-          .body("message.delegator", equalTo(consensusBlsKeys.get(0).getPublicKey().toHexString()))
-          .body(
-              "signature",
-              resp -> {
-                final String messageProxy = resp.path("message.proxy");
-                final String delegator = resp.path("message.delegator");
-                final Bytes32 hashTreeRoot =
-                    new ProxyDelegation(delegator, messageProxy)
-                        .toMerkleizable(scheme)
-                        .hashTreeRoot();
-                final Bytes32 signingRoot = SIGNING_ROOT_GENERATOR.computeSigningRoot(hashTreeRoot);
-                return new ValidBLSSignatureMatcher(delegator, signingRoot);
-              });
-    }
+    // object root is data to sign
+    final Bytes32 objectRoot = Bytes32.random(new Random(0));
+    // signature is calculated on signing root
+    final Bytes32 signingRoot = SIGNING_ROOT_GENERATOR.computeSigningRoot(objectRoot);
 
-    // verify we can get the public keys containing the generated proxy keys
-    final Response pubKeyResponse = signer.callCommitBoostGetPubKeys();
+    final Response response =
+        signer.callCommitBoostRequestForSignature(signRequestType.name(), pubKey, objectRoot);
 
-    pubKeyResponse
+    response
         .then()
-        .log()
-        .body()
         .statusCode(200)
         .contentType(ContentType.JSON)
-        .body("keys[0].consensus", equalTo(consensusBlsKeys.get(0).getPublicKey().toHexString()))
-        .body("keys[0].proxy_bls", hasSize(1))
-        .body("keys[0].proxy_ecdsa", hasSize(1));
+        .body(
+            signRequestType == CommitBoostSignRequestType.PROXY_ECDSA
+                ? new ValidK256SignatureMatcher(pubKey, signingRoot)
+                : new ValidBLSSignatureMatcher(pubKey, signingRoot));
   }
 
-  private static Spec getSpec(Eth2Network eth2Network) {
+  private static Spec getMainnetSpec() {
     final Eth2NetworkConfiguration.Builder builder = Eth2NetworkConfiguration.builder();
-    return builder.applyNetworkDefaults(eth2Network).build().getSpec();
+    return builder.applyNetworkDefaults(Eth2Network.MAINNET).build().getSpec();
   }
 }
